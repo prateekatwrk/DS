cd80d56823f1c4e99e3512696acfba0ef881066e
prateekatwrk <48288884+prateekatwrk@users.noreply.github.com>
2019-10-30 11:40:24 +0530

    Add files via upload

---
+++ ag dsb1/d13 stat3/d11 statistics 1 and statistics 2/data_processing_templete_Inclass.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr  6 14:24:59 2019
+
+@author: Prateek
+"""
+
+# Data Preprocessing Template
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Data.csv')
+X = dataset.iloc[:, :-1]
+X=X.iloc[:,1:].values
+y = dataset.iloc[:, 3].values
+
+
+
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
+sc_y = StandardScaler()
+y_train = sc_y.fit_transform(y_train)
+
+# Taking care of missing data
+from sklearn.preprocessing import Imputer
+imputer = Imputer(missing_values = 'NaN', strategy = 'mean', axis = 0)
+imputer = imputer.fit(X[:, 0:2])
+X[:, 0:2] = imputer.transform(X[:, 0:2])
+
+
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+"""PRATEEK"""
